bf43f1d3c4ee4cedf38158cac32d172508e8bbe6
seanysean <seanysean700@gmail.com>
2022-09-14 14:23:16 -0300

    Create the basic template for displaying projects
    Also changed the global hue to be controlled by a sin wave with a narrower range

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 window.addEventListener('scroll',e=>{
-    //debugger;
-    //const advancedMath = ((window.scrollY * 100) / window.innerHeight) / 100;
     if (window.scrollY < window.innerHeight) {
         const slope = -0.0056;
         const convertHeightToPercentage = 100 * window.scrollY / window.innerHeight;
         const op = Math.abs((convertHeightToPercentage * -0.01));
-        console.log(convertHeightToPercentage);
         if (convertHeightToPercentage > 70) {
             document.querySelector('.section-1 .header').style.opacity = '0';
+            document.querySelectorAll('.project-image').forEach(img=>{
+                img.style.opacity = 1;
+            });
         } else {
             document.querySelector('.section-1 .header').style.opacity = '1';
         }
@@ -17,14 +17,21 @@ window.addEventListener('scroll',e=>{
     }
 });
 
-const hueOffset = 38,
-      sectionOne = document.querySelector('.section-1'),
-      root = document.querySelector(':root');
-let hue = 156;
+const root = document.querySelector(':root');
+let time = 0;
 setInterval(()=>{
     window.requestAnimationFrame(()=>{
-        hue++;
-        root.style.setProperty('--changingHue',hue);
-        //sectionOne.style.backgroundImage = `linear-gradient(hsl(${hue}, 79%, 74%),hsl(${hue + hueOffset}, 38%, 65%))`;
+        time++;
+        const convertTimeToHue = Math.round(97 * Math.sin(time / 180) + 168);
+        root.style.setProperty('--changingHue',convertTimeToHue);
     });
 },200);
+
+document.querySelectorAll('.project-container').forEach(project=>{
+    project.addEventListener('mouseover',()=>{
+        project.classList.add('open');
+    });
+    project.addEventListener('mouseleave',()=>{
+        project.classList.remove('open');
+    })
+});
